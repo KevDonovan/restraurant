a5b632878c5a606362672f59393d96c45def423b
Kevin Donovan <kevdonovan88@gmail.com>
2023-10-26 22:04:15 -0500

    added scrolling and resizing functionality to slideshow

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
-let slideshow = document.getElementsByClassName('slideImages');
+let slideWindow = document.getElementsByClassName('slideImages');
 let backBtn = document.getElementById('backBtn');
 let nextBtn = document.getElementById('nextBtn');
+let slideNum = 0;
+let slideImages = document.getElementsByClassName('imageSlide')
 
 nextBtn.addEventListener('click', ()=> {
-    slideshow[0].scrollRight += 100;
+    if(slideNum < 3) slideNum++;
+    let viewWidth = document.getElementById('imageSlide').offsetWidth;
+    slideWindow[0].style.scrollBehavior = "smooth";
+    slideWindow[0].scrollLeft += viewWidth;
 });
+
+backBtn.addEventListener('click', ()=> {
+    if(slideNum > 0) slideNum--;
+    let viewWidth = document.getElementById('imageSlide').offsetWidth;
+    slideWindow[0].style.scrollBehavior = "smooth";
+    slideWindow[0].scrollLeft -= viewWidth;
+});
+
+function scrollToImg() {
+    slideWindow[0].scrollTo(slideImages[slideNum].offsetLeft, slideImages[slideNum].offsetTop);
+}
+
+window.onresize = scrollToImg;
