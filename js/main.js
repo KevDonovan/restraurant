e8383a84889c6fd06aaa9d82ec95eefbb5e06b8b
Kevin Donovan <kevdonovan88@gmail.com>
2023-11-02 22:29:33 -0500

    improved autoscroll on gallery

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ let backBtn = document.getElementById('backBtn');
 let nextBtn = document.getElementById('nextBtn');
 let slideNum = 0;
 let slideImages = document.getElementsByClassName('imageSlide')
+let scrollInterval = null;
 
 nextBtn.addEventListener('click', ()=> {
     if(slideNum < slideImages.length - 1) slideNum++;
@@ -20,14 +21,15 @@ function smoothScroll() {
         top: slideImages[slideNum].offsetTop,
         behavior: "smooth",
     });
+    autoScroll();
 }
 
 function autoScroll() {
-    setTimeout(() => {
+    clearInterval(scrollInterval);
+    scrollInterval = setInterval(() => {
         if(slideNum === 3) slideNum = 0;
         else slideNum++;
         smoothScroll();
-        autoScroll();
     }, 6000);
 }
 
